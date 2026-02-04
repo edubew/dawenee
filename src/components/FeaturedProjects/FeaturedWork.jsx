30104d8e90666619bc42dfa540182e05c135b16e
edubew <edubew98@gmail.com>
2026-02-04 09:15:58 +0300

    Add the FeaturedWork component that fetchs the data and uses .map() to loop through the array and return each projectcard data

---
+++ src/components/FeaturedProjects/FeaturedWork.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+import projects from "../../data/projects";
+import "./FeaturedWork.scss";
+
+function FeaturedWork() {
+  return (
+    <section className="featured-work">
+      <div className="container">
+        <h2 className="featured-work__title">Featured Projects</h2>
+
+        <div className="featured-work__grid">
+          {projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default FeaturedWork;
