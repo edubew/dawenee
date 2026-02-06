41d53dfdf0db8586529bd855b5dbed9a10e74e4f
edubew <edubew98@gmail.com>
2026-02-06 06:02:13 +0300

    Create the hero slides data file

---
+++ src/data/heroSlides.js
@@ -0,0 +1,23 @@
+import hero1 from "../assets/images/hero/hero-1.jpg";
+import hero2 from "../assets/images/hero/hero-2.jpg";
+import hero3 from "../assets/images/hero/hero-3.jpg";
+
+const heroSlides = [
+  {
+    id: 1,
+    image: hero1,
+    alt: "Elegant balloon backdrop with floral details",
+  },
+  {
+    id: 2,
+    image: hero2,
+    alt: "Thoughtful birthday surprise room ballon decorations",
+  },
+  {
+    id: 3,
+    image: hero3,
+    alt: "Soft and intimate picnic setup",
+  },
+];
+
+export default heroSlides;
