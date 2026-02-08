ea2eeef67ad5db91bb73921dd3f7a4ba5da3129a
edubew <edubew98@gmail.com>
2026-02-08 06:35:05 +0300

    Add the CTA banner component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import FeaturedWork from "../components/FeaturedProjects/FeaturedWork";
 import Services from "../components/Services/Services";
+import Cta from "../components/Cta/Cta";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero />
       <FeaturedWork />
       <Services />
+      <Cta />
     </div>
   );
 };
