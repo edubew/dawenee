35b9e3f3df5e646d86ba230c80587bb9232eff39
edubew <edubew98@gmail.com>
2026-02-12 23:58:04 +0300

    feat: Display all 12 portfolio projects in grid layout - import portfolioProjects data from portfolioData.js - Map over projects to render projects cards - Display project image, title and event type - Create responsive 3 column grid and add other card styling

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import { portfolioProjects } from "../../data/portfolioData";
 import "./Portfolio.scss";
 
 function Portfolio() {
@@ -21,15 +22,26 @@ function Portfolio() {
 
         <section className="portfolio__grid">
           <div className="container">
-            <p
-              style={{
-                textAlign: "center",
-                padding: "4rem 0",
-                color: "#8a7e7a",
-              }}
-            >
-              Projects coming soon...
-            </p>
+            <div className="portfolio__projects">
+              {portfolioProjects.map((project) => (
+                <article key={project.id} className="portfolio__card">
+                  <div className="portfolio__card-image-wrapper">
+                    <img
+                      src={project.media}
+                      alt={project.title}
+                      className="portfolio__card-image"
+                    />
+                  </div>
+
+                  <div className="portfolio__card-content">
+                    <h3 className="portfolio__card-title">{project.title}</h3>
+                    <p className="portfolio__card-event-type">
+                      {project.eventType}
+                    </p>
+                  </div>
+                </article>
+              ))}
+            </div>
           </div>
         </section>
       </main>
