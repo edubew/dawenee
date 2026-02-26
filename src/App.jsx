34997643ef4504099a1003a275f4a93b6600840c
edubew <edubew98@gmail.com>
2026-02-26 14:40:24 +0300

    feat: Simplified the services section - Reduce from detailed cards to visual teasers - Show 6 Key services in a grid layout - Add a CTA to quote calculator

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Booking from "./pages/Booking/Booking";
 import Portfolio from "./pages/Portfolio/Portfolio";
-import Services from "./pages/Services/Services";
+// import Services from "./pages/Services/Services";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/booking" element={<Booking />} />
       <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/services" element={<Services />} />
+      {/* <Route path="/services" element={<Services />} /> */}
       <Route path="/quote" element={<Home />} />
     </Routes>
   );
--- src/components/Services/ServiceCard.jsx
+++ src/components/Services/ServiceCard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./ServiceCard.scss";
+import { Link } from "react-router-dom";
 
 function ServiceCard({ service }) {
   const { title, description, image, icon } = service;
   return (
-    <div className="service-card">
-      <div className="service-card__image">
-        <img src={image} alt={title} />
+    <div className="service-preview-card">
+      <div className="service-preview-card__image-wrapper">
+        <img src={image} alt={title} className="service-preview-card__image" />
 
-        <div className="service-card__icon">{icon}</div>
+        <div className="service-preview-card__icon">{icon}</div>
       </div>
-
-      <div className="service-card__content">
-        <h3 className="service-card__title">{title}</h3>
-        <p className="service-card__description">{description}</p>
+      <div className="service-preview-card__content">
+        <h3 className="service-preview-card__title">{title}</h3>
+        <p className="service-preview-card__description">{description}</p>
       </div>
     </div>
   );
