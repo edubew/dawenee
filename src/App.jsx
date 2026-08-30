576ba804c1ef85d2320de65b5c6d20982336f58c
edubew <edubew98@gmail.com>
2026-08-30 21:03:43 +0300

    Add whatsapp messaging app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { FaWhatsapp } from "react-icons/fa";
 import Home from "./pages/Home/Home";
 import Contact from "./pages/Contact/Contact";
 import Quote from "./pages/Quote/Quote";
@@ -8,17 +9,34 @@ import QuoteConfirmation from "./pages/QuoteConfirmation/QuoteConfirmation";
 import Payment from "./pages/Payment/Payment";
 import SharedQuote from "./pages/SharedQuote/SharedQuote";
 
+const whatsappNumber = "254715784287";
+const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(
+  "Hello Dawenee Decor & Events, I would like to know more about your services."
+)}`;
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/quote" element={<Quote />} />
-      <Route path="/quote/review" element={<QuoteReview />} />
-      <Route path="/quote/confirmation" element={<QuoteConfirmation />} />
-      <Route path="/quote/payment" element={<Payment />} />
-      <Route path="/quote/view" element={<SharedQuote />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/quote/review" element={<QuoteReview />} />
+        <Route path="/quote/confirmation" element={<QuoteConfirmation />} />
+        <Route path="/quote/payment" element={<Payment />} />
+        <Route path="/quote/view" element={<SharedQuote />} />
+      </Routes>
+
+      <a
+        className="whatsapp-widget"
+        href={whatsappUrl}
+        target="_blank"
+        rel="noreferrer"
+        aria-label="Chat with Dawenee on WhatsApp"
+      >
+        <FaWhatsapp />
+      </a>
+    </>
   );
 }
 
